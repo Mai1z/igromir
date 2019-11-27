61678bece5aaee3aa1647713715f85251a54df49
Prytkin Artem <prytkinart@mail.ru>
2019-11-27 18:34:12 +0400

    Добавил доработки в главную страницу

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,29 @@
-$(function(){
-  $('.main-slider__inner').slick({
-    dots: false,
-    arrows: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-  });
 
-  document.querySelector('.header-inner-mobile__burg').onclick = function() {
-    this.classList.toggle('active');
-  }
+$('.main-slider__inner').slick({
+  dots: false,
+  speed: 300,
+  slidesToShow: 1,
+  arrows: false,
+});
+
+function toggleMenu() {
+  $('.header-wrapper__btn').toggleClass('closewrap');
+  $('.header-mobmenu').toggleClass('activewrap');
+  $('.header-inner-mobile__burg').toggleClass('active');
+  $('body').toggleClass('fixed');
+}
+
+$('.header-inner-mobile__burg, .header-wrapper__btn, .header-mobmenu').on('click', toggleMenu);
+$('.header-wrapper').on('click', function(e) {
+  e.stopPropagation();
+  return 0;
+});
+
+$('.catalog__btn').on('click', function() {
+  $('.header-wrapper__inner').toggleClass('show');
+  $('.header-wrapper__catalog').toggleClass('show');
+});
+
+$('.footer-inner__links').on('click', function() {
+  $(this).toggleClass('show');
 });
